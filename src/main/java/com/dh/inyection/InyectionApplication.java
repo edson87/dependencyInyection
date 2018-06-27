8b3f1d1d8f414e7834bdcd5819e4aba7fec27e4f
edson lopez <edson_lopez@hotmail.com.ar>
2018-06-26 22:15:20 -0400

    first commit

---
+++ src/main/java/com/dh/inyection/InyectionApplication.java
@@ -0,0 +1,12 @@
+package com.dh.inyection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InyectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InyectionApplication.class, args);
+    }
+}
